600cb9bed2183de9cb2fafc7679d593c0b35fdc9
unknown <moisesjuniorcas20@gmail.com>
2024-09-16 18:58:06 -0500

    probando otra confi para chromewebdriver

--- src/main/java/mvc/moiso/security/Config.java
+++ src/main/java/mvc/moiso/security/Config.java
@@ -37,7 +37,7 @@ public class Config extends WebSecurityConfigurerAdapter{
                 .antMatchers("/VerEmpleados/**").hasRole("ADMIN")
                 .antMatchers("/Empresa/**").hasRole("ADMIN")
                 .antMatchers("/Empleado/**").hasRole("ADMIN")
-                .antMatchers("/VerMovimientos/**").hasAnyRole("ADMIN","USER")
+                .antMatchers("/VerMovimientos/**").hasAnyRole("ADMIN")
                 .antMatchers("/AgregarMovimiento/**").hasAnyRole("ADMIN","USER")
                 .antMatchers("/AgregarEmpleado/**").hasAnyRole("ADMIN","USER")
                 .antMatchers("/EditarMovimiento/**").hasAnyRole("ADMIN","USER")
